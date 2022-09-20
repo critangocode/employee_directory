3ede8a4da1c038279a115c4983453e617236de0d
ctdevcode <ctdevcode@gmail.com>
2022-09-20 03:16:23 -0600

    CRUD actions setup, and associated views created

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,22 +1,53 @@
 class EmployeesController < ApplicationController
+	before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
   def index
+		@employees = Employee.all
   end
 
   def new
+		@employee = Employee.new
   end
 
   def create
+		@employee = Employee.create(employee_params)
+		if @employee.save
+			flash[:notice] = "Employee created successfully!"
+			redirect_to @employee
+		else
+			flash[:alert] = "There was an issue creating this employee"
+			render 'new'
+		end
   end
 
   def edit
   end
 
   def update
+		if @employee.update(employee_params)
+			flash[:notice] = "Employee updated successfully!"
+			redirect_to @employee
+		else
+			render 'edit'
+		end
   end
 
   def destroy
-  end
+		@employee.destroy
+		flash[:notice] = "Employee was deleted"
+		redirect_to root_path, status: :see_other
+	end
+
+	private
+
+		def set_employee
+			@employee = Employee.find(params[:id])
+		end
+
+		def employee_params
+			params.require(:employee).permit(:first_name, :last_name, :email, :bio)
+		end
 end
