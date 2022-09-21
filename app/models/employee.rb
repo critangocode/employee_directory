cf0e36d268154f3e6b3e98978225bb46ce398ddb
ctdevcode <ctdevcode@gmail.com>
2022-09-20 19:19:48 -0600

    employee model validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+	before_save { first_name.capitalize! }
+	before_save { last_name.capitalize! }
+
+	validates :first_name, presence: true, length: { maximum: 30 }
+	validates :last_name, presence: true, length: { maximum: 30 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+	validates :bio, presence: true, length: { maximum: 300 }
+
 end
