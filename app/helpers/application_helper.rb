4ad5185849aa76765b63ac3034d420143969de61
ctdevcode <ctdevcode@gmail.com>
2022-09-21 18:37:49 -0600

    Gravatar helper method created, gravitar applied to show page and index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def gravatar_for(employee, options = {size: 80})
+		email_address = employee.email.downcase
+		hash = Digest::MD5.hexdigest(email_address)
+		size = options[:size]
+		gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+		image_tag(gravatar_url, alt: employee.first_name)
+	end
 end
