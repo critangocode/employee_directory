882d1b471c31aa7d2c662d44fdc369ffc0bc3e77
ctdevcode <ctdevcode@gmail.com>
2022-09-25 03:42:49 -0600

    Happy path for creating, updating, and deleting from show. Need to fix deleting from index

--- test/system/employees_test.rb
+++ test/system/employees_test.rb
@@ -4,11 +4,49 @@ class EmployeesTest < ApplicationSystemTestCase
 
 	setup do
 		sign_in users(:regular)
+		visit root_url
+
+		@employee = employees(:employee)
 	end
 
-  test "visiting the index" do
-    visit employees_url
+  test "happy path for creating employee" do
+    click_on "New Employee"
+		fill_in "First Name:", with: "Test"
+		fill_in "Last Name:", with: "User"
+		fill_in "Email:", with: "testuser@example.com"
+		fill_in "Biography:", with: "This is a system test for happy path on employee create action"
+		click_on "Create Employee"
   
-    assert_selector "h1", text: "Employees"
+    assert_selector "p", text: "Employee created successfully!"
   end
+
+	test "happy path for updating employee" do
+		click_on "View"
+		click_on "Edit"
+		fill_in "First Name:", with: "Test -edited"
+		fill_in "Last Name:", with: "User -edited"
+		fill_in "Email:", with: "testuser+edited@example.com"
+		fill_in "Biography:", with: "This is a system test for happy path on employee update action"
+		click_on "Update Employee"
+
+		assert_selector "p", text: "Employee updated successfully!"
+	end
+
+	test "happy path for deleting employee from show page" do
+		click_on "View"
+		page.accept_confirm do
+      click_on "Delete"
+    end
+
+		assert_selector "p", text: "Employee was deleted"
+	end
+
+	# test "happy path for deleting employee from index page" do
+	# 	page.accept_confirm do
+  #     click_on "Delete", match: :first 
+  #   end
+
+	# 	assert_selector "p", text: "Employee was deleted"
+	# end
+
 end
